d907d37efd385053f5ee994c514e128a09cccf01
raymond zhao <raymnzd@gmail.com>
2020-04-04 01:55:06 -0400

    emoji

---
+++ src/bin/emoji.rs
@@ -0,0 +1,10 @@
+use std::iter;
+
+fn main(){
+
+    //char is 4 bytes instead of 1
+    let emoji = "🌀";
+    let repeated: String  = iter::repeat(emoji).take(5).collect();
+
+    println!("wow rust has emojis {}", repeated);
+}
---
+++ src/emoji.rs
@@ -0,0 +1,8 @@
+fn main(){
+
+    //char is 4 bytes instead of 1
+    let emoji = "🌀";
+
+    let repeat =  emoji.repeat(4);
+    println!("wow rust has emojis: {}", repeat);
+}
