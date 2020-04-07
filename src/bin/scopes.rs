4702c7ecd0a3a8b346b1cdc30869dcbb9296ab62
raymond zhao <raymnzd@gmail.com>
2020-04-06 21:46:01 -0400

    scopes

---
+++ src/bin/scopes.rs
@@ -0,0 +1,14 @@
+fn main(){
+
+    let x = "hello";
+    {
+        let y = "bye";
+
+        println!("{}, {}",x, y);
+    }
+    println!("{}",x);
+//    ERROR
+//    x still in scope but y is not
+//    println!("{}",y);
+
+}
