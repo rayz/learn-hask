f29d971f2a9f47016aaf87aee703b75ed21ba10a
raymond zhao <raymnzd@gmail.com>
2020-04-07 23:54:16 -0400

    ownership

---
+++ src/bin/own_ref.rs
@@ -0,0 +1,40 @@
+
+
+fn main(){
+
+    let mut s1 = String::from("hello");
+
+    /*
+
+        passing in arg will move the ownership of the object
+        to avoid, pass in the reference of the object instead
+
+    */
+    let len = calculate_length(&s1);
+
+    println!("{}'s length is {}", s1, len);
+
+    change(&mut s1);
+
+    println!("after change, s1 is : {}",s1);
+
+
+    let mut s2 = String::from("test");
+    let r1 = &mut s2;
+
+    let mut s3 = String::from("test2");
+    let r2 = &mut s3;
+    println!("r1 is {} , r2 is {}", r1, r2);
+
+
+}
+
+fn calculate_length(some_string: &String) -> usize{
+
+    some_string.len()
+
+}
+
+fn change(some_string: &mut String){
+    some_string.push_str(" world!");
+}
